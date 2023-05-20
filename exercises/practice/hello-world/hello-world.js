7bfc3a5e8d3482e39764ce9b4c9730e90b68ddb6
MrRango017 <121457030+MrRango017@users.noreply.github.com>
2023-05-20 11:32:39 +0530

    Update hello-world.js

--- exercises/practice/hello-world/hello-world.js
+++ exercises/practice/hello-world/hello-world.js
@@ -1,8 +1,4 @@
-//
-// This is only a SKELETON file for the 'Hello World' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
-export function hello() {
-  return 'Goodbye, Mars!';
+function hello() {
+  return 'Hello World!';
 }
+console.log(hello());
